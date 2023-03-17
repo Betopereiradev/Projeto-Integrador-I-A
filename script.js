aa6574ded02c77396326018811539373bc8a95b3
Alberto Pereira <106534698+Betopereiradev@users.noreply.github.com>
2023-03-17 10:38:49 -0300

    Projeto pronto
    Projeto finalizado.

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
-const modal = document.querySelectorAll(".modal");
-const botaoAbrir = document.querySelectorAll(".abrirModal");
-const botaoFechar = document.querySelectorAll(".fecharModal");
-
-botaoAbrir.forEach((abrirModal) => {
-  abrirModal.addEventListener("click", function () {
-    const modalId = this.getAttribute("data-modal-id");
-    const modal = document.getElementById(modalId);
-    modal.classList.add("aberto");
-  });
-});
-
-botaoFechar.forEach((fecharModal) => {
-  fecharModal.addEventListener("click", function () {
-    const modal = this.closest(".modal");
-    modal.classList.remove("aberto");
-  });
-});
-
 function filtrar() {
-  // Obtém o valor digitado pelo usuário
+  // Pega a entrada no campo de pesquisa
   const query = document.getElementById("filtro").value.toLowerCase();
 
-  // Obtém todos os cards da página
+  // Pega os cards
   const cards = document.getElementsByClassName("card");
 
-  // Percorre todos os cards e verifica se eles correspondem à pesquisa
+  // Loop para percorrer todos os cards e pegar os relacionados à pesquisa
   for (let i = 0; i < cards.length; i++) {
     const card = cards[i];
     const titulo = card.querySelector(".card-titulo").textContent.toLowerCase();
@@ -33,7 +14,7 @@ function filtrar() {
       .textContent.toLowerCase();
     const matches = titulo.includes(query) || conteudo.includes(query);
 
-    // Oculta ou exibe o card conforme a correspondência com a pesquisa
+    // Mostra ou esconde o card de acordo com a pesquisa
     card.style.display = matches ? "" : "none";
   }
 }
